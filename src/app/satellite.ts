a25ffe859176759afc7fab194f0be31409b2994e
Cameron Hogue <cameron@Camerons-MacBook-Pro.local>
2020-01-04 10:05:28 -0600

    Finished assn. 6

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -12,5 +12,14 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     } 
+
+    shouldShowWarning() {
+        if (this.type.toLowerCase() === ("space debris")) {
+            return true;
+        }
+            else {
+                return false;
+            }
+    }
     
 }
